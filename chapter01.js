ee563741eeb151123084575748d7bb4d932cfda3
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2023-10-14 09:34:01 +0800

    initial commit

---
+++ chapter01.js
@@ -0,0 +1,16 @@
+// Printing to the console in javascript
+console.log("this text will be printed");
+
+// Javascript Comments
+// this code prints a greeting
+console.log("hello world");
+
+// Multi-line comments
+/*
+  the code found below
+  will print 'Hello, World!'
+  to the console
+*/
+console.log("Hello, World!");
+
+// Semicolon are optional in javascript
